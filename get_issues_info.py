dc8757cabe50b21880bf7df4405cfc45b49b62a1
t-tellro <roberto.tellez@globant.com>
2020-11-06 19:51:53 -0600

    Executing Python Script

---
+++ get_issues_info.py
@@ -0,0 +1,34 @@
+import requests
+import shutil
+import json
+
+#REPO = "GitHub_Actions"
+REPO = "Dynamo"
+#USER = "RobertGlobant20"
+USER = "DynamoDS"
+
+url = "https://api.github.com/repos/"+USER+"/"+REPO+"/issues"
+    
+read_issues = 1
+page_number = 1
+pull_requests = []
+issues = []
+
+while read_issues > 0:
+    req = requests.get(url, params={'page': str(page_number),  'per_page':'100'},)
+    #print("Response Status:"+str(req.status_code))
+    data  = json.loads(req.content)
+    read_issues = len(data)   
+    for issue in data:
+        issue_type = "issue"
+        if "pull_request" in issue:
+            issue_type = "pull request"
+            pull_requests.append(issue)
+        else:
+            issues.append(issue)
+        #print(str(issue["number"])+ " " + issue_type)        
+    page_number = page_number + 1
+    #print(" ")
+
+print("pull requests: "+str(len(pull_requests)))
+print("issues: "+str(len(issues)))
