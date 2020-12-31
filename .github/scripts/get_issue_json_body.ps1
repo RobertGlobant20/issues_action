3a7d3879d9f4f0a21dae57abf72b40e8f37165be
t-tellro <roberto.tellez@globant.com>
2020-12-31 17:40:30 -0600

    Fixing workflow88

--- .github/scripts/get_issue_json_body.ps1
+++ .github/scripts/get_issue_json_body.ps1
@@ -1,13 +1,18 @@
 param([int]$issueNumber, [string]$issueTitle, [string]$issueDescription)
 
+$issueNumber = 12
+$issueTitle = "Title"
+$issueDescription = "Description"
+
 $json_object = @{ 
 				Number = $issueNumber 
 				Title = $issueTitle
 				Description = $issueDescription
 			} 
 
-$json_string = ConvertTo-Json $json_object
+$json_string = ConvertTo-Json -Compress $json_object
 
-$json_string.Replace('\"','\\\"')
+$json_string = $json_string -replace '"', '\"'
 
 Write-Output $json_string
+
