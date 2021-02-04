aa1474ba59042a1185c946a027708e79e96e36d9
t-tellro <roberto.tellez@globant.com>
2021-02-04 12:09:51 -0600

    Fixing workflow118

--- .github/scripts/issue_body_cleaner.ps1
+++ .github/scripts/issue_body_cleaner.ps1
@@ -24,20 +24,12 @@ function RemoveSpecialCharacters($InputString)
 	return $InputString
 }
 
-#$Request = "https://api.github.com/repos/DynamoDS/Dynamo/issues/11323"
-#$Response = Invoke-WebRequest -URI $Request | 
-#			ConvertFrom-Json | 
-#			Select-Object number, title, body
-
-#$issueContent = $Response.body
-
 $TitleFilters = @("Dynamo version".ToLower(),
 				 "Operating system".ToLower(),
 				 "Stack Trace".ToLower())
 #Parse the template and issue
 $parsed_issue_content = Get_Parsed_Issue $issueContent $TitleFilters
 
-#$issueContent -replace '', ''
 
 $parsed_issue_content | ForEach-Object { 
 	$content = $_['Content']
